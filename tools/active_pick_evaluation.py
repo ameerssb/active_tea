b1e81f96d5ad071527d21e705bdcc4ba4c73a4d0
Sani Bature <sanisbature17@gmail.com>
2026-04-21 13:04:39 +0100

    fully added the new algorithm

--- tools/active_pick_evaluation.py
+++ tools/active_pick_evaluation.py
@@ -7,62 +7,22 @@ import hashlib
 
 
 # -------------------------
-# Utility: Normalize
-# -------------------------
-def norm_dict(_dict):
-    if len(_dict) == 0:
-        return _dict
-    v_max = max(_dict.values())
-    for k, v in _dict.items():
-        _dict[k] = v / (v_max + 1e-6)
-    _dict['max'] = v_max
-    return _dict
-
-
-# -------------------------
-# Load inference results
-# -------------------------
-def PreprocessData(file_name):
-    print('Loading File {}...'.format(file_name))
-    with open(file_name, 'r') as f:
-        data = json.load(f)
-
-    difficult_indicators = defaultdict(float)
-    information_indicators = defaultdict(float)
-    diversity_indicators = defaultdict(float)
-
-    for image_path, boxes_info in data.items():
-        _difficult = 0
-        _information = 0
-        _cls_set = set()
-
-        for box in boxes_info:
-            _information += box['confidence score']
-            _difficult = box['entropy']
-            _cls_set.add(box['pred class'])
-
-        _diversity = len(_cls_set)
-
-        difficult_indicators[image_path] = _difficult
-        information_indicators[image_path] = _information
-        diversity_indicators[image_path] = _diversity
-
-    return data, difficult_indicators, information_indicators, diversity_indicators
-
-
-# -------------------------
 # Count-Min Sketch
 # -------------------------
 class CountMinSketch:
-    def __init__(self, width=2000, depth=5):
+    def __init__(self, width=2000, depth=5, decay=0.99):
         self.width = width
         self.depth = depth
+        self.decay = decay
         self.table = np.zeros((depth, width))
 
     def _hash(self, key, i):
-        return int(hashlib.md5((str(key)+str(i)).encode()).hexdigest(), 16) % self.width
+        return int(hashlib.md5((str(key) + str(i)).encode()).hexdigest(), 16) % self.width
 
-    def update(self, key, value=1):
+    def apply_decay(self):
+        self.table *= self.decay
+
+    def update(self, key, value):
         for i in range(self.depth):
             idx = self._hash(key, i)
             self.table[i][idx] += value
@@ -72,90 +32,83 @@ class CountMinSketch:
 
 
 # -------------------------
-# 🔥 YOUR FULL ALGORITHM
+# Load inference results
 # -------------------------
-def LearnedSketchSelection(data,
-                           difficult_indicators,
-                           information_indicators,
-                           diversity_indicators,
-                           file_name):
+def PreprocessData(file_name):
+    print(f'Loading File {file_name}...')
+    with open(file_name, 'r') as f:
+        data = json.load(f)
+    return data
 
-    print("Running LearnedSketchSelection...")
 
-    # -------- PARAMETERS --------
-    B = 1000       # total memory
-    Br = 200       # heavy bucket capacity
-    threshold = 0.6  # entropy threshold
+# -------------------------
+# 🔥 LBFE WITH TEMPORAL DECAY
+# -------------------------
+def LBFE_Selection(data, file_name):
 
-    unique_buckets = {}   # exact storage (heavy hitters)
-    sketch = CountMinSketch(width=B - Br, depth=5)
+    print("Running LBFE + Temporal Decay...")
+
+    # Parameters
+    sketch = CountMinSketch(width=2000, depth=5, decay=0.99)
 
     final_scores = {}
 
-    # Normalize indicators (optional but useful)
-    difficult_indicators = norm_dict(difficult_indicators)
-    information_indicators = norm_dict(information_indicators)
-    diversity_indicators = norm_dict(diversity_indicators)
-
-    # -------- STREAM PROCESS --------
+    # -------- PASS 1: LEARN FREQUENCY WITH DECAY --------
     for image_path, boxes_info in data.items():
 
-        # "frequency" = number of uncertain detections
-        freq = 0
+        # 🔑 Apply temporal decay before each new image
+        sketch.apply_decay()
+
         for box in boxes_info:
-            if box['entropy'] > threshold:
-                freq += 1
+            cls = box['pred class']
+            conf = box['confidence score']
+            entropy = box['entropy']
 
-        # -------- Learned Oracle --------
-        if freq > 0:   # HH(i) = 1
+            # 🔑 Confidence-aware + uncertainty-aware update
+            value = conf * entropy
 
-            if image_path in unique_buckets:
-                unique_buckets[image_path] += freq
+            sketch.update(cls, value)
 
-            else:
-                if len(unique_buckets) < Br:
-                    unique_buckets[image_path] = freq
-                else:
-                    # Replace smallest bucket (optional)
-                    min_key = min(unique_buckets, key=unique_buckets.get)
-                    if unique_buckets[min_key] < freq:
-                        del unique_buckets[min_key]
-                        unique_buckets[image_path] = freq
+    # -------- PASS 2: SCORE IMAGES --------
+    for image_path, boxes_info in data.items():
 
-        else:
-            # -------- SketchAlg --------
-            sketch.update(image_path, 1)
+        score = 0
 
-    # -------- MERGE RESULTS --------
-    for image_path in data.keys():
-        if image_path in unique_buckets:
-            final_scores[image_path] = 2.0 + unique_buckets[image_path]
-        else:
-            final_scores[image_path] = sketch.query(image_path)
+        for box in boxes_info:
+            cls = box['pred class']
+            entropy = box['entropy']
+
+            # Query frequency
+            freq = sketch.query(cls)
+
+            # 🔑 Rare-class boosting
+            weight = 1.0 / (np.log(freq + 1.0) + 1e-6)
+
+            score += entropy * weight
+
+        final_scores[image_path] = score
 
     # -------- SAVE OUTPUT --------
+    os.makedirs(os.path.dirname(file_name), exist_ok=True)
+
     with open(file_name + '.txt', 'w') as f:
-        for image_path, score in final_scores.items():
-            f.write(str(score) + '\n')
+        for image_path in final_scores:
+            f.write(str(final_scores[image_path]) + '\n')
 
     with open(file_name + '.json', 'w') as f:
         json.dump(final_scores, f)
 
-    print('Finish {}'.format(file_name))
+    print(f'Finish {file_name}')
 
 
 # -------------------------
-# Main wrapper
+# Wrapper
 # -------------------------
 def special_(args):
+    data = PreprocessData(file_name=args.static_file)
 
-    data, difficult_indicators, information_indicators, diversity_indicators = PreprocessData(file_name=args.static_file)
-
-    LearnedSketchSelection(
+    LBFE_Selection(
         data,
-        difficult_indicators,
-        information_indicators,
-        diversity_indicators,
         file_name=args.indicator_file
     )
 
@@ -164,7 +117,7 @@ def special_(args):
 # Entry point
 # -------------------------
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='score function')
+    parser = argparse.ArgumentParser(description='LBFE score function')
 
     parser.add_argument(
         "--static-file",
@@ -175,7 +128,7 @@ if __name__ == '__main__':
     parser.add_argument(
         "--indicator-file",
         type=str,
-        default='results/coco/custom_learned_sketch'
+        default='results/coco/lbfe_scores'
     )
 
     args = parser.parse_args()
